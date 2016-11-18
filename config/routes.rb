590e3daa7c00e2826f13429d05c699e0a8638cf3
Kong Xiong <kong@wisvis.com>
2016-11-18 17:46:45 -0600

    Adds user sign in/up/out and authorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
+
+  root 'users#index'
+
+  resources :users
+  resources :sessions
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
